795b55510b2fd456446ac6717f4d41b471b35fa4
Nicholas Rising <nicholas.rising@gmail.com>
2022-03-10 01:20:48 -0500

    Refactor basically everything

--- covidinc.py
+++ covidinc.py
@@ -1,98 +1,106 @@
-import random
-import pygame
 import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
+import pygame
+import random
 
+INFECT_RATE = 0.1
+DEATH_RATE = 0.01
 
-#recolors the pygame images for given state
-def recolor(state_num,new_color):
-    var = pygame.PixelArray(states[state_num])
-    var.replace(state_colors[state_num], new_color)
-    state_colors[state_num] = new_color
-    del var
+class State:
+    def __init__(self, name, pop, density, image):
+        self.name = name
+        self.pop = pop
+        self.healthy_pop = pop
+        self.sick_pop = 0
+        self.immune_pop = 0
+        self.dead_pop = 0
+        self.density = density
+        self.image = image
 
-#creates the current color of the state based on it's sick and dead populations
-def color_generator(state):
-    redness = 255-int((int(state_values[state][2])/state_values[state][0])*255)
-    darkness = 1-(state_values[state][3]/state_values[state][0])
-    redness = int(redness*darkness)
-    return (int(255*darkness),redness,redness)
+    def update(self):
+        if self.sick_pop < 100:
+            for i in range(self.sick_pop):
+                if random.random() < INFECT_RATE and self.healthy_pop > 0:
+                    self.infect(1)
+        else:
+            infected = int(self.sick_pop * random.random() * INFECT_RATE)
+            if infected > self.healthy_pop:
+                infected = self.healthy_pop
+            self.infect(infected)
+        
+        if self.sick_pop < 100:
+            for i in range(self.sick_pop):
+                if random.random() < DEATH_RATE and self.sick_pop > 0:
+                    self.kill(1)
+        else:
+            dead = int(self.sick_pop * random.random() * DEATH_RATE)
+            if dead > self.sick_pop:
+                dead_pop = self.sick_pop
+            self.kill(dead)
 
-pygame.init()
- 
-size = (960, 540)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Covid Inc.")
+    def infect(self, count):
+        self.sick_pop += count
+        self.healthy_pop -= count
 
-done = False
-clock = pygame.time.Clock()
+    def kill(self, count):
+        self.dead_pop += count
+        self.sick_pop -= count
 
+    def get_healthy_pop(self):
+        return self.pop - self.sick_pop - self.dead_pop
 
-#states[] are the pygame image files for each state
-states_folder = "map_images/map_base"
-states = [pygame.transform.scale(pygame.image.load(os.path.join(states_folder,filename)), size) for filename in os.listdir(states_folder)]
-border = pygame.transform.scale(pygame.image.load("map_images/background.png"), size)
+    def get_color(self):
+        redness = int((1 - self.dead_pop / self.pop) * 255)
+        brightness = int((1 - self.sick_pop / self.pop) * redness)
+        return (redness, brightness, brightness)
 
-
-#current color of each state image
-state_colors = [(255,255,255) for i in range(50)]
-
-
-
-
-# [total population, density, sick population, dead population, infection pool]
-state_values = [[0,0,0,0,0] for i in range(50)]
-
-
-#setting up state_values for each state based on the data
-info = [[int(i.split(',')[0]),float(i.split(',')[1])] for i in open("state_data.csv").readlines()]
-for state in range(50):
-    state_values[state][0] = info[state][0]
-    state_values[state][1] = info[state][1]
-    state_values[state][4] = info[state][0]
-
-
-infection_rate = 2
-death_rate = 0.03
-
-#infect random state
-#state_values[random.randint(0,49)][2] = 1
-state_values[4][2] = 1
-
-while not done:
-    screen.fill((150,200,255))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-
-
-    for state in range(50):
-        state_values[state][2] += 0.3*(1-state_values[state][2]/state_values[state][4])*state_values[state][2]
-        #Final formula to implement: 0.3*(1/(4*% masked+1))*ln(ln(pop density))*(1-infections/infection pool)*infections
-        #If pop density < 16 people/mi^2, then instead of ln(ln(d)), just use 1.
-
-        if state_values[state][2]>state_values[state][0]:
-            state_values[state][2] = state_values[state][0]
-
-        died = state_values[state][2]*death_rate
-        state_values[state][3] += died
-        state_values[state][2] -= died
-        state_values[state][4] -= (died + state_values[state][2])
-
-        #5th Value Vaxxed? Subtracts from infection pool, which reduces infection rate and total possible infected.
-        #Could be implemented without a 5th value.
-
-        #Maybe for each state infected, there's a 10-20% chance another state gets an infection. Can include already infected states.
-
-        if state_values[state][3]>state_values[state][0]:
-            state_values[state][3] = state_values[state][0]
-        recolor(state,color_generator(state))
-
-
-    screen.blit(border,(0,0))
-    for state in states:    
-        screen.blit(state,(0,0))
+def update_map(screen, states, border):
+    screen.fill((150, 200, 255))
+    for state in states:
+        state.image.fill(state.get_color(), special_flags = pygame.BLEND_MIN)
+        screen.blit(state.image, (0, 0))
+    screen.blit(border, (0, 0))
     pygame.display.flip()
-    clock.tick(60)
-pygame.quit()
+
+def main():
+    pygame.init()
+    resolution = (960, 540)
+    screen = pygame.display.set_mode(resolution)
+    pygame.display.set_caption("COVID Inc.")
+    done = False
+    clock = pygame.time.Clock()
+
+    states = []
+    state_folder = "map_images/map_base"
+    for line, filename in zip(open("state_data.csv").readlines(), sorted(os.listdir(state_folder))):
+        name = filename[0].upper() + filename[1:-4]
+        space_index = name.find('_')
+        if space_index != -1:
+            name = name[:space_index] + ' ' + name[space_index + 1].upper() + name[(space_index + 2):]
+        data = line.split(',')
+        image = pygame.transform.scale(pygame.image.load(os.path.join(state_folder, filename)), resolution)
+        states.append(State(name, int(data[0]), float(data[1]), image))
+    border = pygame.transform.scale(pygame.image.load("map_images/background.png"), resolution)
+
+    first_state = states[random.randrange(50)]
+    first_state.infect(1)
+    print(f'A mysterious illness begins in {first_state.name}')
+
+    tick = 0
+    while not done:
+        for state in states:
+            state.update()
+
+        if tick % 10 == 0:
+            update_map(screen, states, border)
+        tick += 1
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
